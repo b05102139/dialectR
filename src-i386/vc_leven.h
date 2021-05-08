eb169cf5999be8086b1275ace0d40c5f791eaf92
Ryan Windows <b05102139@ntu.edu.tw>
2021-05-09 05:42:45 +0800

    Preliminary function documentation

--- src-i386/vc_leven.h
+++
@@ -1,7 +0,0 @@
-#ifndef VC_LEVEN_H
-#define VC_LEVEN_H
-
-#include <Rcpp.h>
-Rcpp::NumericVector vc_leven(Rcpp::StringVector vec1, Rcpp::StringVector vec2, bool alignment_normalization, Rcpp::Nullable<std::string> delim);
-
-#endif
